9e61c28176f44a915c397b8bac2131b31dce5278
Yoga Setiawan <ariokidev@gmail.com>
2025-04-19 17:25:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Tulis konfigurasi nginx saat runtime
 cat <<EOF > /etc/nginx/conf.d/default.conf
-log_format json_logs escape=json '{"time":"\$time_local","ip":"\$remote_addr","path":"\$uri","params":"\$args","body":"\$request_body","status":"\$status","user_agent":"\$http_user_agent"}';
+log_format json_logs escape=json '{"timestamp":"\$time_iso8601","level":"INFO","message":"INBOUND_REQUEST","status":"\$status","path":"\$uri","method":"\$request_method","ip":"\$remote_addr","params":"\$args","body":"\$request_body","user_agent":"\$http_user_agent"}';
 
 access_log /dev/stdout json_logs;
 error_log /dev/stderr;
