9ab77e393e4d8ee81ae79841f4fa1081fd06e7f2
Yoga Setiawan <ariokidev@gmail.com>
2025-04-19 17:17:41 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ server {
   # Log only for /api/w/ and /api/r/ routes
   location ~ ^/api/(w|r)/ {
     access_log /dev/stdout json_logs;
-    proxy_pass http://127.0.0.1:8080;
+    proxy_pass http://127.0.0.1:8000;
     proxy_set_header Host $host;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -24,7 +24,7 @@ server {
   # Disable logging for other routes
   location / {
     access_log off;
-    proxy_pass http://127.0.0.1:8080;
+    proxy_pass http://127.0.0.1:8000;
     proxy_set_header Host $host;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
