90ac241ae5d7565800d89a4d5cda97991ce76ac3
Yoga Setiawan <ariokidev@gmail.com>
2025-04-20 05:58:56 +0700

    Update log_response_body.lua

--- log_response_body.lua
+++ log_response_body.lua
@@ -1,7 +1,19 @@
 core.register_action("log_response_body", { "http-res" }, function(txn)
-    local path = txn.sf:path()
+    -- Escape response body
     local res_body = txn:get_var("res.body")
     if res_body then
-        txn:set_var("res.body", res_body:gsub('"', '\\"'))
+        res_body = res_body:gsub('"', '\\"')
+        txn:set_var("res.body", res_body)
+    else
+        txn:set_var("res.body", "")
+    end
+
+    -- Escape request body
+    local req_body = txn:get_var("req.body")
+    if req_body then
+        req_body = req_body:gsub('"', '\\"')
+        txn:set_var("req.body", req_body)
+    else
+        txn:set_var("req.body", "")
     end
 end)
