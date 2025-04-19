a063b54627aa3edc3f3faf664f2a370c2dec56b5
Yoga Setiawan <ariokidev@gmail.com>
2025-04-20 05:19:51 +0700

    Update log_response_body.lua

--- log_response_body.lua
+++ log_response_body.lua
@@ -1,12 +1,7 @@
 core.register_action("log_response_body", { "http-res" }, function(txn)
     local path = txn.sf:path()
-    if path:match("^/api/w/admins/jobs/run/f/u/") or
-       path:match("^/api/w/[^/]+/jobs/run") or
-       path:match("^/api/r") then
-
-        local res_body = txn:get_var("res.body")
-        if res_body then
-            txn:set_var("res.body", res_body:gsub('"', '\\"'))
-        end
+    local res_body = txn:get_var("res.body")
+    if res_body then
+        txn:set_var("res.body", res_body:gsub('"', '\\"'))
     end
 end)
